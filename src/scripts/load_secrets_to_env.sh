fd8ee13a8687cfafa3cc7367d31b0057f636d673
Rob Godfrey <rob.godfrey@gmail.com>
2023-09-20 14:37:36 +0100

    Update load_secrets_to_env.sh

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
-./doppler secrets download -t "${TOKEN}" --no-file --format env --project "$DOPPLER_PROJECT" > dopplerenv
+./doppler secrets download -t "${TOKEN}" --no-file --format env > dopplerenv
 sed -e 's/^/export /' dopplerenv >> "$BASH_ENV"
 # shellcheck disable=SC1090
 source "$BASH_ENV"
