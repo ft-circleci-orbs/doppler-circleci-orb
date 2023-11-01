7850535b3e9a28711b1ca5b5302657ce1ddb5b31
robgodfrey <rob.godfrey@ft.com>
2023-11-01 19:08:38 +0000

    Add --no-read-env option and tess for loading secrets for 2 configs

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
-./doppler secrets download -t "${TOKEN}" --no-file --format env | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT=' > .circleci/.dopplerenv
+
+./doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format env | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT=' > .circleci/.dopplerenv
+
 sed -e 's/^/export /' .circleci/.dopplerenv >> "$BASH_ENV"
 # shellcheck disable=SC1090
 source "$BASH_ENV"
