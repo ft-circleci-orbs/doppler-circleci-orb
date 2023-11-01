80a1d27cf5558f50bd9e256689ac03c0a7ea97f1
robgodfrey <rob.godfrey@ft.com>
2023-11-01 19:55:32 +0000

    Quote variable

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -4,7 +4,7 @@ TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
 
 SECRETS=$(./doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format env)
 
-for secret in $(echo $SECRETS | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT='); do
+for secret in $(echo "$SECRETS" | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT='); do
     echo "export $secret" >> "$BASH_ENV"
 done
 
