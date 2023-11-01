0db08693d900126e576873e6ddec44582b0f47cc
robgodfrey <rob.godfrey@ft.com>
2023-11-01 17:44:54 +0000

    Apply grep -v to download command output

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
-./doppler secrets download -t "${TOKEN}" --no-file --format env > dopplerenv
-sed -e 's/^/export /' dopplerenv | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT=' >> "$BASH_ENV"
+./doppler secrets download -t "${TOKEN}" --no-file --format env | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT=' > .circleci/.dopplerenv
+sed -e 's/^/export /' .circleci/.dopplerenv >> "$BASH_ENV"
 # shellcheck disable=SC1090
 source "$BASH_ENV"
+rm .circleci/.dopplerenv
