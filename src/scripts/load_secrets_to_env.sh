d4723fd923f9951da2941f1b700aae04e88e7ca7
robgodfrey <rob.godfrey@ft.com>
2023-11-01 17:37:58 +0000

    Filter out DOPPLER_PROJECT, DOPPLER_CONFIG and DOPPLER_ENVIRONMENT variables

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
 ./doppler secrets download -t "${TOKEN}" --no-file --format env > dopplerenv
-sed -e 's/^/export /' dopplerenv >> "$BASH_ENV"
+sed -e 's/^/export /' dopplerenv | grep -v '^DOPPLER_PROJECT=\|^DOPPLER_CONFIG=\|^DOPPLER_ENVIRONMENT=' >> "$BASH_ENV"
 # shellcheck disable=SC1090
 source "$BASH_ENV"
