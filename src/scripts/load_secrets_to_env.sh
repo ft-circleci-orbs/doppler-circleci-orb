2bb1fdd518b6a30ebe7bae2d5e356b57a7e02b17
robgodfrey <rob.godfrey@ft.com>
2023-11-02 01:14:54 +0000

    Add current dir ./

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -2,7 +2,7 @@
 
 TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
 
-SECRETS=$(doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format env)
+SECRETS=$(./doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format env)
 
 while IFS= read -r secret; do
   echo "export $secret" >> "$BASH_ENV"
