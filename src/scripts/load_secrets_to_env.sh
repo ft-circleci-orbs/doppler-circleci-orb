c23584edb4254d468479f0b5d9de58245b001de1
robgodfrey <rob.godfrey@ft.com>
2024-01-26 14:49:20 +0000

    Fix shellcheck issues

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -5,9 +5,9 @@ TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
 SECRETS=$(./doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format json)
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-${SCRIPT_DIR}/install_jq.sh
+"${SCRIPT_DIR}"/install_jq.sh
 
-echo -E $SECRETS |
+echo -E "${SECRETS}" |
   jq 'del(.DOPPLER_CONFIG, .DOPPLER_PROJECT, .DOPPLER_ENVIRONMENT)' |
   jq -r 'to_entries[] | "export \(.key)=\"\(.value)\""' >> "$BASH_ENV"
 
