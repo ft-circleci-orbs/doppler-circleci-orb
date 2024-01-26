5bab324a1453b29b1b0343c3a2ae7a0dfc516436
robgodfrey <rob.godfrey@ft.com>
2024-01-26 11:24:22 +0000

    Fix path for python script

--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -4,7 +4,7 @@ TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
 
 SECRETS=$(./doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format json)
 
-python parse_secrets.py "${SECRETS}" >> "$BASH_ENV"
+python ./parse_secrets.py "${SECRETS}" >> "$BASH_ENV"
 
 # shellcheck disable=SC1090
 source "$BASH_ENV"
