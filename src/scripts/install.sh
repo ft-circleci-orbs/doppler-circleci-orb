3ba0211aedd23c4e0a971b6f556b972bcb282e2e
robgodfrey <rob.godfrey@ft.com>
2024-02-15 15:22:38 +0000

    Move install of jq to install script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -21,3 +21,21 @@ fi
 
 # Install doppler cli
 (curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh -s -- --no-install --no-package-manager
+
+# Install jq if its not present
+if ! command -v jq &> /dev/null
+then
+  if command -v apt-get &> /dev/null
+  then
+    apt-get install -y jq
+  elif command -v apk &> /dev/null
+  then
+    apk add jq
+  elif command -v brew &> /dev/null
+  then
+    HOMEBREW_NO_AUTO_UPDATE=1 brew install jq
+  else
+    echo "Unable to install jq. Unable to detect apt-get, apk or homebrew"
+    exit 1
+  fi
+fi
--- src/scripts/load_secrets_to_env.sh
+++ src/scripts/load_secrets_to_env.sh
@@ -4,24 +4,6 @@ TOKEN=$(eval echo "\$$DOPPLER_TOKEN_NAME")
 
 SECRETS=$(./doppler secrets download -t "${TOKEN}" --no-file --no-read-env --format json)
 
-# Install jq if its not present
-if ! command -v jq &> /dev/null
-then
-  if command -v apt-get &> /dev/null
-  then
-    apt-get install -y jq
-  elif command -v apk &> /dev/null
-  then
-    apk add jq
-  elif command -v brew &> /dev/null
-  then
-    HOMEBREW_NO_AUTO_UPDATE=1 brew install jq
-  else
-    echo "Unable to install jq. Unable to detect apt-get, apk or homebrew"
-    exit 1
-  fi
-fi
-
 echo -E "${SECRETS}" |
   jq 'del(.DOPPLER_CONFIG, .DOPPLER_PROJECT, .DOPPLER_ENVIRONMENT)' |
   jq -r 'to_entries[] | "export \(.key)=\"\(.value)\""' >> "$BASH_ENV"
